2fc82e10fee3df3a4665620e32b16faadea24576
unknown <9698108@itisrossi.vi.it>
2023-05-27 10:40:26 +0200

    Non so quale account

---
+++ C/EserciziFile/Es3/Es3.c
@@ -0,0 +1,20 @@
+/* Si realizzi un programma che costruisca un file binario di nome "anagrafe" contenente le informazioni relative ai cittadini di un 
+comune italiano.
+Ogni record del file "anagrafe" rappresenta una persona abitante nel comune e contiene le seguenti informazioni:
+Nome: una stringa che rappresenta il nome della persona;
+Cognome: una stringa che rappresenta il cognome della persona;
+Data di nascita: un insieme di tre interi che rappresenta la data di nascita;
+Luogo di nascita: una stringa che rappresenta la località di nascita;
+Indirizzo: una stringa che rappresenta l'indirizzo della persona;
+Codice Fiscale: una sequenza di 16 caratteri che individua univocamente il cittadino.
+Il programma dovrà riempire il file "anagrafe" con informazioni immesse dall'utente attraverso 
+lo standard input e, successivamente, visualizzarne il contenuto. */
+
+#include <stdio.h>
+
+int main()
+{
+    
+
+    return 0;
+}

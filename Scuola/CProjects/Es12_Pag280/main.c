d5008e21b657078a16e5c9561da48c1a9530a267
Computer-hp <windowsc045@gmail.com>
2023-11-06 18:13:36 +0100

    modified

--- Scuola/CProjects/Es12_Pag280/main.c
+++ Scuola/CProjects/Es12_Pag280/main.c
@@ -1,3 +1,20 @@
+/*
+12 
+Scrivere una funzione in linguaggio C per
+sistema operativo Linux o Windows avente il se-
+guente prototipo
+
+float prodottoScalare (int N, float v[], float w[]);
+
+dove N rappresenta la dimensione dei vettori
+v e w e che restituisca il prodotto scalare tra v
+e w, cioé la somma di prodotti tra gli elementi
+corrispondenti dei due vettori; la funzione deve
+massimizzare le prestazioni nel caso di esecu-
+zione con due CPU disponibili utilizzando due
+thread.
+*/
+
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
@@ -8,7 +25,7 @@
 #define N_THREADS 2
 #define N_PROCESSES 2
 
-struct  Valori
+struct Valori
 {
     float *v;
     float *w;
@@ -25,7 +42,7 @@ void caricaVettore(float array[], int dim)
 
     for (int i = 0; i < dim; i++)
     {
-        array[i] = (float)rand() / RAND_MAX * 100  + 1;
+        array[i] = (float)rand() / RAND_MAX * 100  + 1; // da 1 a 100
     }
 
 }
@@ -56,15 +73,11 @@ float prodottoScalare(float v[], float w[], int dim)
         valori[i].result = 0;
         
         if (i == 0) 
-        {
             valori[i].start = 0;
-            valori[i].end = dim / 2;
-        }
         else 
-        {
             valori[i].start = dim / 2;
-            valori[i].end = dim;
-        }
+
+        valori[i].end = dim / (-(i - 2));
 
         pthread_create(&threads[i], NULL, (void * (*)(void *))calcolo, valori);
     }
@@ -101,6 +114,9 @@ int main()
         return -1;
     }
 
+    // start timer
+    clock_t start_time = clock();
+
     int id = fork();
 
     if (id < 0)
@@ -127,11 +143,14 @@ int main()
         printf("\nIl prodotto scalare = %.2lf", result);
 
         close(fd[0]);
+
+        // end timer
+        clock_t end_time = clock(); // Stop the timer
+        double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+        
+        printf("\nElapsed time: %.2lf seconds\n", elapsed_time);
     }
-
-
     wait(NULL);
-
 
     return 0;
 }

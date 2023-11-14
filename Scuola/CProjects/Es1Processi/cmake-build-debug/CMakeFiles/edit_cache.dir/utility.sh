15cf25cc38b38fd18a7b139b50634a5b5c92a807
Computer-hp <windowsc045@gmail.com>
2023-11-14 13:24:29 +0100

    modified

--- Scuola/CProjects/Es1Processi/cmake-build-debug/CMakeFiles/edit_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /cygdrive/c/Users/AnticHp/Desktop/CProjects/cmake-build-debug
-/cygdrive/c/Users/AnticHp/AppData/Local/JetBrains/CLion2023.2/cygwin_cmake/bin/cmake.exe -E echo No\ interactive\ CMake\ dialog\ available.

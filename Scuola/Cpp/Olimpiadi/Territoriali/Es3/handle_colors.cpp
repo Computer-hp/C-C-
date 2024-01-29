b1dd58334181363bd69722fda938f4abcb445c01
Computer-hp <windowsc045@gmail.com>
2024-01-29 17:55:15 +0100

    modified

--- Scuola/Cpp/Olimpiadi/Territoriali/Es3/handle_colors.cpp
+++ Scuola/Cpp/Olimpiadi/Territoriali/Es3/handle_colors.cpp
@@ -23,15 +23,20 @@ void put_color_in_vector(vector<int> &v, int color_to_add)
 
     int min_color_length = 0;
 
+    vector<int> tmp_colors_on_wall;
+
     while (end < v.size())
     {
+        tmp_colors_on_wall.clear();
+        tmp_colors_on_wall.reserve(v.size());
+        tmp_colors_on_wall.insert(tmp_colors_on_wall.end(), v.begin(), v.end());
+
         int sum_of_colors = 0;
 
         int i = start;
 
-
-// problem:  what to do when the first color is == to 'color_to_add'
-        while (sum_of_colors >= color_to_add && i < v.size())
+    // problem:  what to do when the first color is == to 'color_to_add'
+        while (sum_of_colors < color_to_add && i < v.size())
         {
             sum_of_colors += v[i];
             i++;
@@ -46,14 +51,14 @@ void put_color_in_vector(vector<int> &v, int color_to_add)
         int position_min_color = -1;
         int position_bigger_min_color = -1; //  color > color_to_add and the smallest of that type
 
-        get_min_color_length(v, start, end, &position_min_color, &position_bigger_min_color, color_to_add);
+        get_min_color_length(tmp_colors_on_wall, start, end, &position_min_color, &position_bigger_min_color, color_to_add);
 
         if (position_bigger_min_color != -1)
         {
-            v[position_bigger_min_color] -= color_to_add;
+            tmp_colors_on_wall[position_bigger_min_color] -= color_to_add;
 
             // place left or right the color_to_add
-            place_new_color_right_or_left(v, color_to_add, position_bigger_min_color);
+            place_new_color_right_or_left(tmp_colors_on_wall, color_to_add, position_bigger_min_color);
             continue;
         }
 
@@ -67,30 +72,88 @@ void put_color_in_vector(vector<int> &v, int color_to_add)
             start++;
             continue;
         }
-        
-        place_new_color_at_the_best_side(v, color_to_add, position_min_color, rest);
+
+        // 1° problem: what to do when tmp_colors_on_wall[position_min_color] == 1, because it has to be removed
+        place_new_color_at_the_best_side(tmp_colors_on_wall, color_to_add, position_min_color, rest);
+
+
+// save number of colors at the right or left
+// ...
+
+
+        start++;
     }
 }
 
 
 
+/*
+    places the color in the position where at the other side are more colors
+
+    input:
+        [4, 1, 1, 1, 1]   <----   3
+
+    output:
+        [1, 3, 1, 1, 1]
+
+    error output:
+        [4, 3, 1]  or  [4, 1, 3]
+*/
+
 void place_new_color_at_the_best_side(vector<int> &v, int color_to_add, int position_min_color, int rest)
 {
     if (position_min_color == 0)
     {
+        int right_index = check_colors_at_left_or_right(v, position_min_color, 1, rest);  // right side
+
+        // place the color at the index returned
+        v.erase(v.begin() + position_min_color, v.begin() + right_index + 1);   // '+ 1' to include the color at position 'position_min_color'.
+        v.insert(v.begin() + position_min_color, color_to_add);
 
         return;
     }
 
     if (position_min_color == v.size() - 1)
     {
+        int left_index = check_colors_at_left_or_right(v, position_min_color, -1, rest); // left side
 
+        // place the color at the index returned
+        v.erase(v.begin() + left_index, v.begin() + position_min_color + 1);
+        v.insert(v.begin() + left_index, color_to_add)
         return;
     }
+
+    int right_index = check_colors_at_left_or_right(v, position_min_color, 1, rest);  // right side
+    int left_index  = check_colors_at_left_or_right(v, position_min_color, -1, rest); // left side
+    
+
+
+
 }
 
-int check_colors_at_left_or_right(vector<int> &v, int right_or_left_index, int plus_or_minus, int rest)
+
+
+/*
+
+    Finds, presuming that right_or_left_index is >= '0' && < 'vector (wall) size()' ,
+
+    the position where the color to add has to be placed, returning the index.
+
+    input:
+            [3, 4, 7]    <----   5
+
+    output:
+            [1, 5, 1, 7]
+                ^
+                |
+                |
+            index where color long '5' has been placed
+
+*/
+
+int check_colors_at_left_or_right(vector<int> &v, int position_min_color, int plus_or_minus, int rest)
 {
+    int right_or_left_index = position_min_color + plus_or_minus;
     int right_or_left_rest;
 
     while (right_or_left_index >= 0 && right_or_left_index < v.size())
@@ -98,12 +161,14 @@ int check_colors_at_left_or_right(vector<int> &v, int right_or_left_index, int p
         right_or_left_rest = v[right_or_left_index] - rest;
 
         if (right_or_left_rest >= 0)
-        {
             return right_or_left_index;
-        }
 
         right_or_left_index += plus_or_minus;
+
+        rest = -right_or_left_rest; 
     }
+
+    return -1;  // the color is to big for putting it on the wall from 'start' to 'end'
 }
 
 
@@ -215,7 +280,7 @@ void solve(int t)
 	for (int i : used_colors)
 		result++;
 
-    cout << "Case #" << t << ": " << result << endl;
+    std::cout << "Case #" << t << ": " << result << endl;
 }
 
 

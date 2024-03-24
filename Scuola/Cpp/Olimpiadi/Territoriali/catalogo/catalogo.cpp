fd754caec3b01c49050b34da9ad078380622bf7a
Computer-hp <windowsc045@gmail.com>
2024-03-24 15:23:16 +0100

    modified

--- Scuola/Cpp/Olimpiadi/Territoriali/catalogo/catalogo.cpp
+++ Scuola/Cpp/Olimpiadi/Territoriali/catalogo/catalogo.cpp
@@ -1,61 +1,105 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <algorithm>
+
+
 using namespace std;
 
 
-void aggiungi(vector<int> &v, long long int id);
-void togli(vector<int> &v, long long int id);
-int conta(vector<int> &v, long long int id);
+void aggiungi(long long int id);
+void togli(long long int id);
+int conta(long long int id);
 
 
-int main() {
+vector<vector<long long int>> v;
+
+
+int main() 
+{
     ios::sync_with_stdio(false);
 
-    // Uncomment the following lines if you want to read/write from files
     ifstream cin("input.txt");
     ofstream cout("output.txt");
 
     int Q;
     cin >> Q;
 
-    for(int i = 0; i < Q; i++){
+    for (int i = 0; i < Q; i++)
+    {
         char t;
         long long int id;
         cin >> t >> id;
 
-        if(t == 'a') 
-        {
+        if (t == 'a') 
             aggiungi(id);
-        } 
+            
         else if (t == 't') 
-        {
             togli(id);
-        } 
+
         else if (t == 'c') 
-        {
             cout << conta(id) << '\n';
-        }
     }
 
     return 0;
 }
 
 
-void aggiungi(vector<int> &v, long long int id)
+void aggiungi(long long int id)
 {
+    bool found = false;
 
+    for (auto &v2 : v)
+        if (!v2.empty() && v2[0] == id)
+        {
+            v2.push_back(id);
+            found = true;
+            break;
+        }
+
+
+    if (found)
+        return;
+        
+    vector<long long int> new_v2;
+
+    new_v2.push_back(id);
+
+    auto it = find_if(v.begin(), v.end(), [id](const vector<long long int> &v2) 
+    {
+        return v2.empty() || v2[0] > id;
+    });
+
+    if (it != v.end())  v.insert(it, new_v2);
+
+    else                v.push_back(new_v2);
 }
 
 
-void togli(vector<int> &v, long long int id)
+void togli(long long int id)
 {
+    auto it = find_if(v.begin(), v.end(), [id](const vector<long long int> &v2)
+    {
+        return v2[0] == id;
+    });
 
-
+    if (it != v.end())
+        it -> pop_back();
 }
 
 
-int conta(vector<int> &v, long long int id)
+int conta(long long int id)
 {
+    int counter = 0;
 
+    for (const auto &v2 : v)
+    {
+        if (v2[0] != id)
+            continue;
 
+        counter += count(v2.begin(), v2.end(), id);
+        break;
+    }
+
+    return counter;
 }

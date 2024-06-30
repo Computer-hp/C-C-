9b76cb7bffad44de7e4b7972b4434ba2214629f0
Computer-hp <windowsc045@gmail.com>
2024-06-30 23:46:07 +0200

    modified

---
+++ Scuola/Cpp/Olimpiadi/Territoriali/ois_scrigni/scrigni.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <assert.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+double scosse(int N) 
+{
+    return 42;
+}
+
+
+int main() 
+{
+    FILE *fr, *fw;
+    int N;
+
+    fr = fopen("input.txt", "r");
+    fw = fopen("output.txt", "w");
+    
+    assert(1 == fscanf(fr, "%d", &N));
+    fprintf(fw, "%.6f\n", scosse(N));
+
+    fclose(fr);
+    fclose(fw);
+
+    return 0;
+}
